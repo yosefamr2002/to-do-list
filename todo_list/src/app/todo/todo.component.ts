86ce42b7355def466dcd3dd51e74b40131afff73
YoussefTrust <yosefamr2002@icloud.com>
2024-12-28 21:40:56 +0200

    second

---
+++ todo_list/src/app/todo/todo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo.component.html',
+  styleUrl: './todo.component.css'
+})
+export class TodoComponent {
+
+  imagelink: string
+}
